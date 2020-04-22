783d948e6ca305408a3e68ad15b7cc2c625fb390
lipingd2 <lipingd2@illinois.edu>
2020-04-21 19:26:04 -0500

    Added csv reader

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -9,9 +9,17 @@
 #include <cinder/Vector.h>
 #include <cinder/gl/draw.h>
 #include <cinder/gl/gl.h>
+#include <rapidcsv.h>
+#include <unistd.h>
+#include <gflags/gflags.h>
+using std::cout;
+using std::endl;
+using std::string;
 
 
 namespace myapp {
+
+DEFINE_string(file, "", "the path of a file");
 
 using cinder::app::KeyEvent;
 
@@ -22,14 +30,31 @@ MyApp::MyApp() {
   clearBoard();
 }
 
+// a empty board at the beginning if user do not give the file
 void MyApp::clearBoard() {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) board[i][j] = 0;
   }
 }
 
+// setup the board
 void MyApp::setup() {
   cursor_x = cursor_y = N/2;
+  if (!FLAGS_file.empty()) {
+    try {
+      rapidcsv::Document doc(FLAGS_file,
+          rapidcsv::LabelParams(-1, -1));
+      for (int i = 0; i < N;i++) {
+        std::vector<int> row = doc.GetRow<int>(i);
+        for (int j = 0; j < N; j++) {
+          board[i][j] = row.at(j);
+        }
+      }
+    } catch (std::ios_base::failure) {
+      std::cout << "File not found or csv error\n";
+      exit(1);
+    }
+  }
 }
 
 void MyApp::update() { }
@@ -106,10 +131,15 @@ void MyApp::keyDown(KeyEvent event) {
   }
 }
 
+//to check if it is boundary when do the toggle
 bool MyApp::isInside(int x) const {
   return 0 <= x && x < N;
 }
 
+// toggle a "star" around cursor
+//  +
+// +++
+//  +
 void MyApp::toggle5() {
   const auto toggle = [&](int x, int y) {
     if (isInside(x) && isInside(y)) board[x][y] ^= 1;
@@ -119,5 +149,7 @@ void MyApp::toggle5() {
     toggle(cursor_x + dx4[dir], cursor_y + dy4[dir]);
   }
 }
+
+
 
 }  // namespace myapp
